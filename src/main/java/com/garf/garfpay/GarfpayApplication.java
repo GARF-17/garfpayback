0fcc2a742bc31aea9a199e76790663438faaf640
GARF-17 <garf2117@gmail.com>
2026-07-27 18:44:22 -0500

    Primer commit

---
+++ src/main/java/com/garf/garfpay/GarfpayApplication.java
@@ -0,0 +1,13 @@
+package com.garf.garfpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarfpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarfpayApplication.class, args);
+	}
+
+}
